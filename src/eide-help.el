3cd5c78505956274a003a12dfdee7e8d876b7937
Cédric Marie <cedric.marie@yahoo.fr>
2012-05-11 13:32:49 +0200

    Update hosting information (TuxFamily)

--- src/eide-help.el
+++ src/eide-help.el
@@ -20,7 +20,7 @@
 (provide 'eide-help)
 
 (defvar eide-version "1.7+")
-(defvar eide-release-date "2012-04")
+(defvar eide-release-date "2012-05")
 
 ;;;; ==========================================================================
 ;;;; INTERNAL FUNCTIONS
